82cfdc2069786a7a301a3f67f73560a71191a282
nossamoguh <masson.hugo1508@gmail.com>
2026-04-27 18:52:06 +0200

    Add files via upload

---
+++ projet histoire/index.js
@@ -0,0 +1,3 @@
+var plan = document.createElement("img")
+plan.src = "Louis-Joseph_Mondhare,_Plan_de_la_ville_et_faubourg_de_Paris_divisé_en_ses_48_sections,_1790_-_Gallica.jpg"
+document.body.appendChild(plan)
