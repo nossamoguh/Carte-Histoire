f9f23ce7a7c305c32fdeaebd7e701577473519a6
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-28 21:58:23 +0200

    Update index.js

--- projet histoire/index.js
+++ projet histoire/index.js
@@ -1,3 +1,5 @@
 var plan = document.createElement("img")
-plan.src = "Louis-Joseph_Mondhare,_Plan_de_la_ville_et_faubourg_de_Paris_divisé_en_ses_48_sections,_1790_-_Gallica.jpg"
+plan.src = "Paris1790.jpg"
+plan.style.maxWidth = "100%" // changer selon l'utilisation sur la page voulu, certainement moins que ça
+plan.style.heigth = "auto"
 document.body.appendChild(plan)
