336d34b4097e5fb0a6889d15592e0561cd914222
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-28 22:26:01 +0200

    Update index.js

--- projet histoire/index.js
+++ projet histoire/index.js
@@ -1,5 +1,17 @@
 var plan = document.createElement("img")
 plan.src = "Paris1790.jpg"
-plan.style.maxWidth = "100%" // changer selon l'utilisation sur la page voulu, certainement moins que ça
-plan.style.heigth = "auto"
+
+plan.style.maxWidth = "100%"
+plan.style.height = "auto"
+
+
+window.mouseX = 0;
+window.mouseY = 0;
+function Zoom(element) {
+    
+    window.mouseX = element.x;
+    window.mouseY = element.y;
+
+};
+
 document.body.appendChild(plan)
