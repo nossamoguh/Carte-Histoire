2da99a832be2867ac55f036d0fbf6c53beae92df
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-29 16:41:43 +0200

    Refactor image creation and event listener setup

--- projet histoire/index.js
+++ projet histoire/index.js
@@ -3,15 +3,12 @@ plan.src = "Paris1790.jpg"
 
 plan.style.maxWidth = "100%"
 plan.style.height = "auto"
+document.body.appendChild(plan)
 
 const react = plan.getBoundingClientRect();
-
 
 element.addEventListener("mousemove", (event) => {
     const x = event.clientX - react.left;
     const y = event.clientY - react.right;
     console.log(x, y)
 });
-
-document.body.appendChild(plan)
-
