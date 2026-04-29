cbce0f5d4b604427cb18c3a6645ff677ac991dad
patategrill <194829329+patategrill@users.noreply.github.com>
2026-04-29 16:39:51 +0200

    Update index.js

--- projet histoire/index.js
+++ projet histoire/index.js
@@ -4,14 +4,14 @@ plan.src = "Paris1790.jpg"
 plan.style.maxWidth = "100%"
 plan.style.height = "auto"
 
+const react = plan.getBoundingClientRect();
 
-window.mouseX = 0;
-window.mouseY = 0;
-function Zoom(element) {
-    
-    window.mouseX = element.x;
-    window.mouseY = element.y;
 
-};
+element.addEventListener("mousemove", (event) => {
+    const x = event.clientX - react.left;
+    const y = event.clientY - react.right;
+    console.log(x, y)
+});
 
 document.body.appendChild(plan)
+
